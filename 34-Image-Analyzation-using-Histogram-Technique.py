4cf5bb76161fe8683e776f786ea3f0105a89cf77
Neetigya Maurya <neetigyamaurya2@gmail.com>
2023-10-10 09:07:35 +0530

    Various Histogram Techniques were Added

--- 34-Image-Analyzation-using-Histogram-Technique.py
+++ 34-Image-Analyzation-using-Histogram-Technique.py
@@ -4,6 +4,7 @@
 # Histograms can help extract information about contrast, brightness, and intensity.
 import cv2
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Load an image
 image = cv2.imread('images/ColorWall-dp3lzm.jpg')
@@ -45,6 +46,35 @@ cv2.imshow("Blue Channel", b)
 cv2.imshow("Green Channel", g)
 cv2.imshow("Red Channel", r)
 
+# For gray scale images
+image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+hist = cv2.calcHist([image], [0], None, [256], [0, 255])
+plt.plot(hist)
+plt.title("Grayscale Histogram")
+plt.show()
+
+# Histogram equalization is good when of the images is confined to a particular region. It accepts gray scale image.
+equ = cv2.equalizeHist(image)
+res = np.hstack((image, equ))  # Optional step done so that the original image and the new output are joined
+cv2.imshow("Equalized Histogram", res)
+hist = cv2.calcHist([equ], [0], None, [256], [0, 256])
+plt.plot(hist)
+plt.title("Equalized Histogram")
+plt.show()
+
+# Contrast Limited Adaptive Histogram Equalization (CLAHE)
+# Creates a CLAHE object (Arguments are optional)
+# It is used to enhance image and also handle noise from an image region.
+# Gray scale image is required
+clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8)) # Keep the clipLimit between 1 and two for better results.
+cl1 = clahe.apply(image)
+cv2.imshow("CLAHE", cl1)
+hist = cv2.calcHist([cl1], [0], None, [256], [0, 256])
+res = np.hstack((image, equ, cl1))  # Optional step done so that the original image and the new output are joined
+plt.plot(hist)
+plt.title("CLAHE")
+plt.show()
+cv2.imshow("Comparison", res)
 # Wait for a key press, and if 'q' is pressed, close all windows
 if cv2.waitKey(0) == ord('q'):
     cv2.destroyAllWindows()
